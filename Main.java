d8a4663c9130ba3e0585cdde8b061d78fd999eea
JuanCamiloFVX <61198455+JuanCamiloFVX@users.noreply.github.com>
2020-03-10 22:31:49 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Nota n1 = new Nota(4.0);
+		
+		Materia m1 = new Materia("Fisica",n1);
+		
+		
+		List Materias = new ArrayList();
+		
+		
+		
+	}
+
+}
